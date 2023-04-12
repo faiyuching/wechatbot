d7cf9a91579f4ef351f600c550666925b9bbf678
faiyuching <faiyuching@gmail.com>
2023-04-12 09:46:02 +0800

    fix: 修改掉线通知人

--- api/src/handlers/on-logout.js
+++ api/src/handlers/on-logout.js
@@ -7,15 +7,15 @@ async function onLogout(user) {
     axios.post('https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=8d3a65d0-bb6b-4bb4-96d7-7741b235cc71', {
         msgtype: "text",
         text: {
-            content: "小新登出了！请访问管理后台重新登录",
-            mentioned_list: ["ZuoQiaoXiaoCuo","YuQing"],
+            content: "小澜登出了！请访问管理后台重新登录",
+            mentioned_list: ["KaoLaWu","YuQing","ZuoQiaoXiaoCuo"],
         }
     })
     .then(res => {
-        console.log("通知小新机器人掉线成功!")
+        console.log("通知小澜机器人掉线成功!")
     })
     .catch(error => {
-        console.log("通知小新机器人掉线失败!", error)
+        console.log("通知小澜机器人掉线失败!", error)
     })
 }
 export default onLogout;
