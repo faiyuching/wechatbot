126eb64e1a2c37910f7c9e9b47dc73fe4361bd48
faiyuching <faiyuching@gmail.com>
2023-08-31 02:17:20 +0800

    金数据推送成绩

--- api/src/routes/jinshuju.js
+++ api/src/routes/jinshuju.js
@@ -2,5 +2,5 @@ import * as express from "express";
 import * as jinshujuController from "../controllers/jinshuju.js";
 import auth from "../util/auth.js";
 const router = express.Router();
-router.post('/score', auth.required, jinshujuController.validate.getJinshujuScore, jinshujuController.getJinshujuScore);
+router.post('/score', jinshujuController.validate.getJinshujuScore, jinshujuController.getJinshujuScore);
 export default router;
